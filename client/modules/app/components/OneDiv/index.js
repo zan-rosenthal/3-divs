a6afff3710b7f6fe6c853d21c575bd9dc54db90a
zan-rosenthal <zan.rosenthal@gmail.com>
2018-01-23 00:14:40 -0600

    Bind event handlers

--- client/modules/app/components/OneDiv/index.js
+++ client/modules/app/components/OneDiv/index.js
@@ -6,15 +6,15 @@ const getBackgroundColor = color => `rgb(0,${color * 100},215)`;
 const getDivStyles = ({ index, maxIndex }) => {
   const length = maxIndex * 25 + 20;
   const backgroundColor = getBackgroundColor(index);
+
   return {
-    "align-items": "center",
+    alignItems: "center",
     border: "gray 1px solid",
     backgroundColor,
     color: "white",
     display: "flex",
     height: `${length}%`,
-    "justify-content": "center",
-    position: "absolute",
+    justifyContent: "center",
     width: `${length}%`,
     zIndex: maxIndex + 1
   };
@@ -24,26 +24,28 @@ const getHeaderStyles = ({ index }) => ({
   margin: `${index * 2}.5% 0 auto 0`
 });
 
-export default class OneDiv extends PureComponent {
-  handleClick = () => {
-    console.log("clicked div", this.props.index);
-  };
+const handleMouseDown = (index) =>  (e) => {
+  e.stopPropagation()
+  // eslint-disable-next-line no-alert
+  alert(`You clicked div #${index}`)
+}
 
+export default class OneDiv extends PureComponent {
   render() {
     const { index, maxIndex } = this.props;
+
     return (
       maxIndex > 0 &&
       <div
         className={styles.outerDiv}
-        onClick={this.handleClick.bind(this)}
+        onMouseDown={handleMouseDown(index)}
         style={getDivStyles(this.props)}
       >
         <span style={getHeaderStyles(this.props)}>Div {index}</span>
         <OneDiv
           className={styles.innerDiv}
           index={index + 1}
-          maxIndex={maxIndex - 1}
-        />
+          maxIndex={maxIndex - 1}        />
       </div>
     );
   }
